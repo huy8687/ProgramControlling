beaa8bbeb50320971fccc535093a55245ac02187
Bui Dac Huy <huybuidac@gmail.com>
2016-01-24 09:57:55 +0700

    first commit

---
+++ ProgramControlling/ControllingService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Text;
+
+namespace ProgramControlling
+{
+    public partial class ControllingService : ServiceBase
+    {
+        public ControllingService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        internal void OnDebug()
+        {
+            OnStart(null);
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ ProgramControlling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+
+namespace ProgramControlling
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+#if DEBUG
+            var ser = new ControllingService();
+            ser.OnDebug();
+            Thread.Sleep(System.Threading.Timeout.Infinite);
+#else
+            var servicesToRun = new ServiceBase[] 
+            { 
+                new Service1() 
+            };
+            ServiceBase.Run(servicesToRun);
+#endif
+        }
+    }
+}
